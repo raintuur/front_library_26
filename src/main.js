badc5ca12456eb319f37388f4f405af04625fcec
Rain Tüür <rain.tuur@gmail.com>
2022-11-02 18:54:52 +0200

    Added bootstrap and axios

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import VueAxios from "vue-axios";
+import axios from "axios";
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap.js"
 
+Vue.use(VueAxios, axios);
 Vue.config.productionTip = false
 
 new Vue({

a0e807781989d83b94f984acfa4679f14202def5
Margit Toots <m.toots@hotmail.com>
2022-11-07 22:58:25 +0200

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <template>
   <div id="app">
     <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">Kodu</router-link> |
+      <router-link to="/by-city">Raamatukogu</router-link> |
+      <router-link to="/about">Log-in</router-link>
     </nav>
     <router-view/>
   </div>
---
+++ src/views/LibraryView.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <div class="container">
+      <div class="row justify-content-start">
+        <div class="col col-lg-3">
+          <!--          <button type="button" class="btn btn-outline-primary">Primary</button>-->
+          <button type="button" class="col-xs-12 col-sm-4 text-center res-pad same-width-button btn btn-outline-dark">
+            Kõik
+          </button>
+          <div class="row m-1">
+          </div>
+          <button type="button" class="col-xs-12 col-sm-4 text-center res-pad same-width-button btn btn-outline-dark">
+            Tallinn
+          </button>
+          <div class="row m-1">
+          </div>
+          <button type="button" class="col-xs-12 col-sm-4 text-center res-pad same-width-button btn btn-outline-dark">
+            Tartu
+          </button>
+          <div class="row m-1">
+          </div>
+          <button type="button" class="col-xs-12 col-sm-4 text-center res-pad same-width-button btn btn-outline-dark">
+            Pärnu
+          </button>
+          <div class="row">
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+</template>
+
+<script>
+export default {
+  name: "LibraryView"
+}
+</script>
+
+<style scoped>
+
+</style>

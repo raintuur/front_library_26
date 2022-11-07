7e6409d916a2412aef0e0b76802c13c4785a68af
Mario Mumm <mario.mumm@hotmail.com>
2022-11-07 17:03:22 +0200

    MARIO-FL-2 Created LibrariesView.vue, added buttons and layout

---
+++ src/views/LibrariesView.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="col col-lg-3">
+    <button type="button" class="btn btn-light">Kõik</button>
+    <div class="row m-3"></div>
+    <button type="button" class="btn btn-light">Tallinn</button>
+    <div class="row m-3"></div>
+    <button type="button" class="btn btn-light">Tartu</button>
+    <div class="row m-3"></div>
+    <button type="button" class="btn btn-light">Pärnu</button>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'RaamatukogudView',
+  data: function () {
+    return {
+      cities: [
+        {
+          cityName: '',
+          cityId: 0
+        }
+      ]
+    }
+  },
+  methods: {
+    getCityList: function () {
+      this.$http.get("/some/path")
+          .then(response => {
+            console.log(response.data)
+          })
+          .catch(error => {
+            console.log(error)
+          })
+    },
+  }
+}
+
+</script>

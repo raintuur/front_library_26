46875afe3c3cea8045cefd965cd8b113f2cbf00f
Mairold <mairold.maegi@gmail.com>
2022-11-07 16:09:59 +0200

    MAIROLD-FL-1 > Library page created

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import LibraryView from "@/views/LibraryView";
 
 Vue.use(VueRouter)
 
@@ -9,6 +10,11 @@ const routes = [
     path: '/',
     name: 'home',
     component: HomeView
+  },
+  {
+    path: '/library',
+    name: 'Raamatukogud',
+    component: LibraryView
   },
   {
     path: '/about',
---
+++ src/views/LibraryView.vue
@@ -0,0 +1,13 @@
+<template>
+ <div>Some message</div>
+</template>
+
+<script>
+export default {
+  name: "LibraryView"
+}
+</script>
+
+<style scoped>
+
+</style>

845d5063dece43b9049a44a5d483a4e2a4ce99c8
temp <astrid.oras@gmail.com>
2022-11-07 16:57:45 +0200

    Buttons

---
+++ src/views/LibraryView.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <div>
+      <div class="d-grid gap-2 col-6 mx-auto">
+        <button class="btn btn-primary" type="button">Button</button>
+        <button class="btn btn-primary" type="button">Button</button>
+        <button class="btn btn-primary" type="button">Button</button>
+        <button class="btn btn-primary" type="button">Button</button>
+      </div>
+    </div>
+
+
+
+
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: "LibraryView"
+}
+</script>
+
+<style scoped>
+
+</style>

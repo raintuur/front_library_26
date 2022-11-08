6ee8081b2aed24518e40050c9e193ddb590e3005
Mihke <mihkelviirsalu@hotmail.com>
2022-11-08 11:21:22 +0200

    dropdown config

--- src/components/CityDropdown.vue
+++ src/components/CityDropdown.vue
@@ -2,15 +2,34 @@
  <div>
 
   <select class="form-select" aria-label="Default select example">
-    <option selected disabled value="0">--</option>
-    <option value="1">One</option>
-    <option value="2">Two</option>
-    <option value="3">Three</option>
+    <option selected disabled value="0">Kõik</option>
+    <option v-for="city in cities" :key="city.cityNameID" :value="city.cityNameID">{{city.cityName}}</option>
   </select>
   </div>
 </template>
 <script>
 export default {
-  name: 'CityDropdown'
+  name: 'CityDropdown',
+  data: function () {
+    return {
+      cities: [
+          {
+        cityName:'Tallinn',
+        cityNameID: 15
+      },
+        {
+          cityName:'Tartu',
+          cityNameID: 21
+        },
+        {
+          cityName:'Pärnu',
+          cityNameID: 23
+        },
+      ]
+
+    }
+
+  }
+
 }
 </script>

a4b2a50daf05404d007ce4ea6c49ceb42cbbf2e3
olegtrofimov <olegtrofimov96@gmail.com>
2022-11-08 22:45:22 +0200

    OLEG-FL-2

--- src/components/library_view_table/CityLibraryTable.vue
+++ src/components/library_view_table/CityLibraryTable.vue
@@ -1,53 +1,56 @@
 <template>
   <div>
-
-
     <table class="table table-success table-striped-columns" style=margin-left:100px>
-      <thead>
-      <tr>
-        <th scope=col>Linn</th>
-        <th scope=col>Nimi</th>
-        <th scope=col></th>
-      </tr>
-      </thead>
-      <tbody>
-      <tr>
-        <td>Tallinn</td>
-        <td>Keskraamatukogu</td>
-        <td>
-          <button type="button" class="btn btn-warning">Vaata</button>
-        </td>
-      </tr>
-      <tr>
-        <td>Tallinn</td>
-        <td>Pääskula Raamatukogu</td>
-        <td>
-          <button type="button" class="btn btn-warning">Vaata</button>
-        </td>
-      </tr>
-      <tr>
-        <td>Tartu</td>
-        <td>Tammelinna Raamatukogu</td>
-        <td>
-          <button type="button" class="btn btn-warning">Vaata</button>
-        </td>
-      </tr>
-      <tr>
-        <td>Pärnu</td>
-        <td>Ranna Raamatukogu</td>
-        <td>
-          <button type="button" class="btn btn-warning">Vaata</button>
-        </td>
-      </tr>
 
+            <thead>
+            <tr>
+              <th scope=col>Linn</th>
+              <th scope=col>Nimi</th>
+              <th scope=col> </th>
+            </tr>
+            </thead>
+      <tbody>
+      <tr v-for="citiesAndLibraries in libraries">
+        <td>{{citiesAndLibraries.cityName}}</td>
+        <td>{{citiesAndLibraries.libraryName}}</td>
+        <td><button type="button" class="btn btn-warning">Vaata</button></td>
+
+      </tr>
       </tbody>
     </table>
+
+
 
   </div>
 </template>
 <script>
 export default {
   name: 'CityLibraryTable',
+  data: function () {
+    return{
+      libraries: [
+        {
+          cityName: '',
+          libraryName: '',
+          libraryId: 0
+        }
+      ]
+    }
+  },
+  methods: {
+    getLibraryNames: function () {
+      this.$http.get("/library/city-list/all")
+          .then(response => {
+            this.libraries = response.data
+          })
+          .catch(error => {
+            console.log(error)
+          })
+    },
+  },
+  beforeMount() {
+    this.getLibraryNames()
+  }
 
 }
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -2,11 +2,16 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import LibraryView from "@/views/LibraryView";
+import libraryView from "@/views/LibraryView";
 
 Vue.use(VueRouter)
 
 const routes = [
   {
+    path: '/library/city-list/all',
+    name: 'libraryList',
+    component: libraryView
+  },{
     path: '/',
     name: 'home',
     component: HomeView

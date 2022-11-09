7f1777e31f7bf948c2f7f6351001a8d4d122e697
Andres Orgla <aorgla@gmail.com>
2022-11-09 08:57:51 +0200

    message

--- src/views/LibrariesView.vue
+++ src/views/LibrariesView.vue
@@ -6,6 +6,25 @@
     <div class="col-lg-3">
       <CityButtons/>
     </div>
+    <div class="col-lg-6">
+      <table class="table">
+        <thead>
+        <tr>
+          <th scope="col">Linn</th>
+          <th scope="col">Nimi</th>
+          <th scope="col"></th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>
+            <button class="btn btn-outline-dark">Nupp</button></td>
+        </tr>
+        </tbody>
+      </table>
+    </div>
     
   </div>
 

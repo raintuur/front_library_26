db22fa11100f1205a8daf7fc048b191f8885b4a9
Andres Orgla <aorgla@gmail.com>
2022-11-10 09:35:53 +0200

    FL-4

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
     devServer: {
         proxy: 'https://stoplight.io/mocks/valiit/myproject/25244259'
 
-    //    https://stoplight.io/mocks/
+    //    https://stoplight.io/mocks/  aa
     //    https://stoplight.io/mocks/lambikas/library/25244259
     }
 }

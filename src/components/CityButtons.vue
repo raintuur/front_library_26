d9d1ee7d5942c8735e16b09527971fc639b94397
Kristiina <kristiina.saarniit@yahoo.com>
2022-11-10 22:55:07 +0200

    Table component created

---
+++ src/components/CityButtons.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="d-grid gap-3 col-5 mx-auto">
+    <button class="btn btn-primary" type="button">Kõik</button>
+    <button v-for="city in cities" class="btn btn-primary" type="button">{{city.cityName}}</button>
+
+  </div>
+</template>
+<script>
+export default {
+  name: 'CityButtons',
+  data: function () {
+    return {
+      cities: [
+        {
+          cityName: 'Tallinn',
+          cityId: 15
+        },
+        {
+          cityName: 'Tartu',
+          cityId: 21
+        },
+        {
+          cityName: 'Pärnu',
+          cityId: 23
+        },
+      ]
+    }
+  }
+}
+</script>
---
+++ src/views/LibrariesView.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+
+    <div class="container">
+      <div class="row justify-content-start">
+        <div class="col-2">
+
+          <CityButtons/>
+        </div>
+      </div>
+    </div>
+
+
+  </div>
+</template>
+
+<script>
+import CityButtons from "@/components/CityButtons";
+
+export default {
+  name: "LibrariesView",
+  components: {CityButtons},
+
+</script>
